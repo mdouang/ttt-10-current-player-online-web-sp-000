4664ea67efff4b37b5956fd4941ffc807b73f790
Maikaru <maikaru@maikaru.com>
2021-06-02 23:24:51 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,14 +3,14 @@ require 'pry'
 def turn_count(board)
   counter = 0
   board.each do |token|
-    
    if token == "X" ||token =="O"
     counter += 1
         end
   end
-  binding
   counter
 end
+
+# ^-- integer
 
 def current_player(board)
   if turn_count(board) % 2 == 0
